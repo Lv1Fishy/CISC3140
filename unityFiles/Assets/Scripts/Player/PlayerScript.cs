f54949d2bdca523b19447e783b2a8e34c96107fd
Oscar <oscarhuang6112@gmail.com>
2025-04-16 00:13:50 -0400

    Map update

--- unityFiles/Assets/Scripts/Player/PlayerScript.cs
+++ unityFiles/Assets/Scripts/Player/PlayerScript.cs
@@ -10,19 +10,20 @@
 
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour {
     
     [SerializeField] int HP;
     [SerializeField] float movementSpeed = 10f;
 
-
+    [SerializeField] private Vector2 moveInput;
     [SerializeField] Rigidbody2D body;
     [SerializeField] GameObject inventoryUI; // Reference to the inventory UI object
     [SerializeField] PersistentData instance; // Reference to the PersistentData instance
     void Start() {
         //Setting up Player Stats
-        HP = instance.getCurrentHealth(); // Get the current health from the PersistentData instance
+        /*HP = instance.getCurrentHealth(); // Get the current health from the PersistentData instance*/
         
 
 
@@ -35,7 +36,8 @@ public class PlayerScript : MonoBehaviour {
 
     
     void Update() {
-        PlayerMovement();   //Checks for player input and moves the player accordingly
+        body.linearVelocity = moveInput * movementSpeed;
+        /*PlayerMovement();*/   //Checks for player input and moves the player accordingly
         checkInventory();   // Check for inventory input
         
     }
@@ -47,10 +49,15 @@ public class PlayerScript : MonoBehaviour {
     /*
     * PlayerMovement() - Handles player movement based on input
     */
+    void Move(InputAction.CallbackContext context) {
+         moveInput = context.ReadValue<Vector2>();
+    }
     void PlayerMovement() {
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         body.linearVelocity = new Vector2(moveHorizontal * movementSpeed, moveVertical * movementSpeed);
+        
     }
 
     /*
